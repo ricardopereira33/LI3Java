84307d25100f9643bd85ced9594d67459a16f69c
Dinis Peixoto <Dinis Peixoto>
2016-06-11 01:09:35 +0100

    removido comparator desnecessario

--- ComparatorByString.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-import java.io.Serializable;
-
-public class ComparatorByString implements Comparator<String>, Serializable{
-   public int compare(String a, String b){
-       return a.compareTo(b);
-   }
-}
