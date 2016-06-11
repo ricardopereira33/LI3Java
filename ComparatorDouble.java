52770f314686702e39b4a201c38693284952958c
Ricardo Pereira <ricardo.agp.914@gmail.com>
2016-06-11 19:19:23 +0100

    ta quase

--- ComparatorDouble.java
+++
@@ -1,9 +0,0 @@
-import java.util.Comparator;
-import java.io.Serializable;
-
-public class ComparatorDouble implements Comparator<ParStringDouble>, Serializable{
-    
-   public int compare(ParStringDouble a, ParStringDouble b){
-       return a.getNumero().compareTo(b.getNumero());
-   }
-}
--- ComparatorString.java
+++
@@ -1,8 +0,0 @@
-import java.util.Comparator;
-import java.io.Serializable;
-
-public class ComparatorString implements Comparator<ParStringDouble>, Serializable{
-   public int compare(ParStringDouble a, ParStringDouble b){
-       return a.getString().compareTo(b.getString());
-   }
-}
